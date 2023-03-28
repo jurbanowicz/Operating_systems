14557f9a75600c70bcf76b39dc8eaee129f945a7
Parallels <jurbanowicz@student.agh.edu.pl>
2023-03-28 17:02:00 +0200

    fixed task 3

--- cw04/zad3/catcher.c
+++ cw04/zad3/catcher.c
@@ -5,10 +5,12 @@
 #include <time.h>
 
 int requests_no = 0;
+int current_mode;
+int to_execute = 0;
 
 void write_numbers() {
     for (int i = 1; i <= 100; i++) {
-        printf("%d", i);
+        printf("%d\n", i);
     }
 }
 void get_current_time() {
@@ -21,46 +23,51 @@ void get_current_time() {
 }
 
 void get_requests_num(){
-    printf("%d", requests_no);
+    printf(">>>CATCHER: Numebr of requests received: %d\n", requests_no);
 }
 
 void get_time_loop() {
-    while(1) {
+    while(current_mode == 4) {
         get_current_time();
         sleep(1);
     }
+    to_execute = 1;
 }
 
 void execute_mode(int work_mode) {
     switch (work_mode) {
         case 1:
             write_numbers();
+            to_execute = 0;
             break;
         case 2:
             get_current_time();
+            to_execute = 0;
             break;
         case 3:
             get_requests_num();
+            to_execute = 0;
             break;
         case 4:
             get_time_loop();
             break;
         case 5:
-            printf("CATCHER: ending program\n");
+            printf(">>>CATCHER: ending program\n");
             exit(0);
         default:
-            fprintf(stderr, "Incorrect signal value\n");
-            break;
+            fprintf(stderr, "Incorrect work mode value\n");
     }
+
 }
 
 void handler(int sign, siginfo_t* info, void* c){
     pid_t sender_pid = info->si_pid;
-    printf(">>>CATHCER: Signal %d received\n", sign);
+    current_mode = info->si_status;
+    printf(">>>CATCHER: Signal %d received\n", sign);
     requests_no++;
     kill(sender_pid, SIGUSR1);
-    printf(">>>CATCHER: signal sent\n");
-    execute_mode(info->si_status);
+    printf(">>>CATCHER: signal confirmation sent to sender\n");
+    to_execute = 1;
 }
 
 int main(void) {
@@ -70,11 +77,12 @@ int main(void) {
     act.sa_flags = SA_SIGINFO;
     sigaction(SIGUSR1, &act, NULL);
 
-    printf("%d\n", getpid());
+    printf(">>>CATCHER: starting program -> PID: %d\n", getpid());
 
     while (1) {
-
+        if (to_execute) {
+        execute_mode(current_mode);
+        }
     }
-
     return 0;
 }
